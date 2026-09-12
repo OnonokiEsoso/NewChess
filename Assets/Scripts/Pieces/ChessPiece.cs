ee37d2ed12b77314d13adb9d61a97697ec9809ec
OnonokiEsoso <yuta114yuta@gmail.com>
2026-09-13 00:01:32 +0900

    Prepare chess pieces for board-controlled movement

--- Assets/Scripts/Pieces/ChessPiece.cs
+++ Assets/Scripts/Pieces/ChessPiece.cs
@@ -1,25 +1,21 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class ChessPiece : MonoBehaviour
 {
     public int BoardX { get; private set; }
     public int BoardY { get; private set; }
     public bool IsSelected { get; private set; }
-
-    private static ChessPiece selectedPiece;
+    public bool HasMoved { get; private set; }
 
     [Header("Selection")]
     [SerializeField] private Color selectedColor = Color.yellow;
 
     private SpriteRenderer spriteRenderer;
-    private Collider2D pieceCollider;
     private Color normalColor;
 
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        pieceCollider = GetComponent<Collider2D>();
 
         if (spriteRenderer != null)
         {
@@ -27,36 +23,10 @@ public class ChessPiece : MonoBehaviour
         }
     }
 
-    private void Update()
-    {
-        if (Mouse.current == null || !Mouse.current.leftButton.wasPressedThisFrame)
-        {
-            return;
-        }
-
-        if (pieceCollider == null)
-        {
-            return;
-        }
-
-        Camera mainCamera = Camera.main;
-        if (mainCamera == null)
-        {
-            return;
-        }
-
-        Vector2 mouseScreenPosition = Mouse.current.position.ReadValue();
-        Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(mouseScreenPosition);
-
-        if (pieceCollider.OverlapPoint(mouseWorldPosition))
-        {
-            ToggleSelection();
-        }
-    }
-
     public void Initialize(int boardX, int boardY)
     {
         SetBoardPosition(boardX, boardY);
+        HasMoved = false;
     }
 
     public void SetBoardPosition(int boardX, int boardY)
@@ -67,41 +37,18 @@ public class ChessPiece : MonoBehaviour
         gameObject.name = $"{GetType().Name}_{boardX}_{boardY}";
     }
 
-    private void ToggleSelection()
+    public void MarkMoved()
     {
-        if (selectedPiece == this)
-        {
-            Deselect();
-            selectedPiece = null;
-            return;
-        }
-
-        if (selectedPiece != null)
-        {
-            selectedPiece.Deselect();
-        }
-
-        Select();
-        selectedPiece = this;
+        HasMoved = true;
     }
 
-    private void Select()
+    public void SetSelected(bool selected)
     {
-        IsSelected = true;
+        IsSelected = selected;
 
         if (spriteRenderer != null)
         {
-            spriteRenderer.color = selectedColor;
-        }
-    }
-
-    private void Deselect()
-    {
-        IsSelected = false;
-
-        if (spriteRenderer != null)
-        {
-            spriteRenderer.color = normalColor;
+            spriteRenderer.color = selected ? selectedColor : normalColor;
         }
     }
 }
