59ff965575f7c466836b2690e35d1dbdb1d415d0
OnonokiEsoso <yuta114yuta@gmail.com>
2026-09-12 23:44:07 +0900

    Add piece click selection

--- Assets/Scripts/Pieces/ChessPiece.cs
+++ Assets/Scripts/Pieces/ChessPiece.cs
@@ -4,6 +4,25 @@ public class ChessPiece : MonoBehaviour
 {
     public int BoardX { get; private set; }
     public int BoardY { get; private set; }
+    public bool IsSelected { get; private set; }
+
+    private static ChessPiece selectedPiece;
+
+    [Header("Selection")]
+    [SerializeField] private Color selectedColor = Color.yellow;
+
+    private SpriteRenderer spriteRenderer;
+    private Color normalColor;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (spriteRenderer != null)
+        {
+            normalColor = spriteRenderer.color;
+        }
+    }
 
     public void Initialize(int boardX, int boardY)
     {
@@ -16,5 +35,43 @@ public class ChessPiece : MonoBehaviour
         BoardY = boardY;
 
         gameObject.name = $"{GetType().Name}_{boardX}_{boardY}";
+    }
+
+    private void OnMouseDown()
+    {
+        if (selectedPiece == this)
+        {
+            Deselect();
+            selectedPiece = null;
+            return;
+        }
+
+        if (selectedPiece != null)
+        {
+            selectedPiece.Deselect();
+        }
+
+        Select();
+        selectedPiece = this;
+    }
+
+    private void Select()
+    {
+        IsSelected = true;
+
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.color = selectedColor;
+        }
+    }
+
+    private void Deselect()
+    {
+        IsSelected = false;
+
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.color = normalColor;
+        }
     }
 }
