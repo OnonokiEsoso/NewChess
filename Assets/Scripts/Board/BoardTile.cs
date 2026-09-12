31f615cfecae209baeea7349e960871b18ee2751
OnonokiEsoso <yuta114yuta@gmail.com>
2026-09-12 23:22:04 +0900

    Add alternating board tile colors

--- Assets/Scripts/Board/BoardTile.cs
+++ Assets/Scripts/Board/BoardTile.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class BoardTile : MonoBehaviour
 {
     public int X { get; private set; }
     public int Y { get; private set; }
 
-    public void Initialize(int x, int y)
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void Initialize(int x, int y, Color tileColor)
     {
         X = x;
         Y = y;
 
         gameObject.name = $"Tile_{x}_{y}";
+        spriteRenderer.color = tileColor;
     }
 }
