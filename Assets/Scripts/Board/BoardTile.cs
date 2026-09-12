4eb0a6f5e058c012a4c93a347c4c5642041a1318
OnonokiEsoso <yuta114yuta@gmail.com>
2026-09-13 00:01:24 +0900

    Add move highlighting to board tiles

--- Assets/Scripts/Board/BoardTile.cs
+++ Assets/Scripts/Board/BoardTile.cs
@@ -5,8 +5,13 @@ public class BoardTile : MonoBehaviour
 {
     public int X { get; private set; }
     public int Y { get; private set; }
+    public bool IsHighlighted { get; private set; }
+
+    [Header("Highlight")]
+    [SerializeField] private Color moveHighlightColor = new Color(0.35f, 0.9f, 0.35f, 1f);
 
     private SpriteRenderer spriteRenderer;
+    private Color normalColor;
 
     private void Awake()
     {
@@ -17,8 +22,15 @@ public class BoardTile : MonoBehaviour
     {
         X = x;
         Y = y;
+        normalColor = tileColor;
 
         gameObject.name = $"Tile_{x}_{y}";
-        spriteRenderer.color = tileColor;
+        spriteRenderer.color = normalColor;
+    }
+
+    public void SetMoveHighlight(bool highlighted)
+    {
+        IsHighlighted = highlighted;
+        spriteRenderer.color = highlighted ? moveHighlightColor : normalColor;
     }
 }
